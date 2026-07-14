05be3c4dedfcb136e285faa4fd61c82809d81d04
Alfred Ngwayah <96799441+Big-Logic@users.noreply.github.com>
2026-07-14 09:18:33 +0000

    feat: (sprint1) implement CRUD for courses (#6)
    ## Files added
    1. courses.routes.js
    2. courses.controller.js
    3. course.service.js
    4. courses.validators.js
    
    ## Files updated
    1. routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const authRoutes = require('./auth.routes');
 const userRoutes = require('./users.routes');
 const programRoutes = require('./programs.routes');
+const courseRoutes = require('./courses.routes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/programs', programRoutes);
+router.use('/courses', courseRoutes);
 
 module.exports = router;
