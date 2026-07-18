d03595acd2267045b6be498a70c5e0a92d53d2d4
Alfred Ngwayah <96799441+Big-Logic@users.noreply.github.com>
2026-07-18 00:18:47 +0000

    feat: (sprint1) Updates users api (#8)
    Functionalities include: create, deactivate, and reactivate user accounts

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const SALT_ROUNDS = 10;
 
@@ -10,4 +11,17 @@ function comparePassword(plainPassword, passwordHash) {
   return bcrypt.compare(plainPassword, passwordHash);
 }
 
-module.exports = { hashPassword, comparePassword };
+function generateRandomPassword(length = 12) {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let password = '';
+
+  while (password.length < length) {
+    const byte = crypto.randomBytes(1)[0];
+    const index = byte % charset.length;
+    password += charset[index];
+  }
+
+  return password;
+}
+
+module.exports = { hashPassword, comparePassword, generateRandomPassword };
